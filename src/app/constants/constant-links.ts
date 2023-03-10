b7639ca9c1afab9ae679df90fc3b7d4e984e4dde
josuedavidgodinez <godinezjosue@hotmail.com>
2023-03-10 17:51:43 -0600

    add education link

--- src/app/constants/constant-links.ts
+++ src/app/constants/constant-links.ts
@@ -1,23 +1,29 @@
 
 export const PrincipalLinks = [
     {
-        name: 'About me',
+        name: 'About me',  //Personal text about me
         link : "./aboutme"  
     },
     {
-        name: 'Career',
+        name: 'Career', //About my expirience
         link : "./career"  
     },
     {
-        name: 'Abilities',
-        link : "./career"  
-    },
-    {
-        name: 'Projects',
-        link : "./aboutme"  
-    },
-    {
-        name: 'Contact Me',
+        name: 'Education', //About my Education and Courses
         link : "./contact-me"  
-    }
+    },
+    {
+        name: 'Abilities', // abilities measured with starts
+        link : "./career"  
+    },
+    {
+        name: 'Projects', // Projects where I worked
+        link : "./aboutme"  
+        
+    },
+    {
+        name: 'Contact Me',  //Form and links to contact with me
+        link : "./contact-me"  
+    },
+
 ]
